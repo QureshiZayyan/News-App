f399a08df48a047e4fac97b5b11f5903b0b27169
Zayyan <qureshizayyan49@gmail.com>
2024-09-03 01:43:36 +0530

    added

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,5 +1,6 @@
 import { useState, createContext } from "react";
 const StateContext = createContext();
+console.log('this is a context');
 
 function StateProvider({ children }) {
   const [data, setData] = useState([]);
